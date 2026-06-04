f6020ed6ce70b16484a006e0fea52ac33ae98f31
SOham <soham.a.vade@gmail.com>
2026-06-04 12:21:39 +0530

    Codes

---
+++ C_programs/Competitive/Assignment 11/Q5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void RangeDisplay(int iStart, int iEnd)
+{
+    int iCnt = 0;
+    for(iCnt = iEnd; iCnt >= iStart; iCnt--)
+    {
+        printf("%d\t", iCnt);
+    }
+}
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+    printf("Enter Starting point : ");
+    scanf("%d", &iValue1);
+
+    printf("Enter Ending point : ");
+    scanf("%d", &iValue2);
+    
+    RangeDisplay(iValue1, iValue2);
+    return 0;
+}
