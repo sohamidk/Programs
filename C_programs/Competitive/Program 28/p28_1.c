1c1aadfd15fa069b754ab6995634bf5b40b2892d
SOham <soham.a.vade@gmail.com>
2026-06-19 15:56:14 +0530

    C Codes

---
+++ C_programs/Competitive/Program 28/p28_1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+        if(iRow != iCol)
+    {
+        printf("Invalid input\n");
+        printf("Numbers of rows and colomns should be equal\n");
+        return;
+    }
+    for(i = 1; i<= iRow; i++)
+    {
+        for(j = iCol; j>=1; j--)
+        {
+           if(i == j)
+           {
+            printf("#\t");
+           }
+           else 
+           {
+            printf("*\t");
+           }
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows : ");
+    scanf("%d", &iValue1);
+
+    printf("Enter number of coloumns : ");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+    return 0;
+}
