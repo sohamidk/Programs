5c1aec4a9f1821f5968c07b6363ee2c02784774d
SOham <soham.a.vade@gmail.com>
2026-07-08 19:09:47 +0530

    Doubly LL

---
+++ C_programs/Data_Structure/program387.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+
+struct node
+{
+    int data;
+    struct node * next;
+    struct node * prev;     // $
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+int Count(PNODE first)
+{
+    return 0;
+}
+
+
+void Display(PNODE first)
+{}
+
+
+void InsertFirst(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+    newn->prev = NULL;      //$
+
+    if(NULL == *first)      //LL is Empty
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        (*first)->prev = newn;      //$
+        *first = newn;
+    }
+}
+
+
+void InsertLast(PPNODE first, int iNo)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = iNo;
+    newn->next = NULL;
+    newn->prev = NULL;      //$
+
+    if(NULL == *first)      //LL is Empty
+    {
+        *first = newn;
+    }
+    else
+    {}
+}
+
+
+void InsertAtPos(PPNODE first, int iNo, int iPos)
+{}
+
+
+void DeleteFirst(PPNODE first)
+{}
+
+
+void DeleteLast(PPNODE first)
+{}
+
+
+void DeleteAtPos(PPNODE first, int iPos)
+{}
+
+
+int main()
+{
+    PNODE head = NULL;
+
+    InsertFirst(&head,51);
+    InsertFirst(&head,21);
+    InsertFirst(&head,11);
+
+    
+    return 0;
+}
