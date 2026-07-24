c18ac2241a892c29adc4dec1f0863e8e67c4320a
SOham <soham.a.vade@gmail.com>
2026-07-24 18:53:52 +0530

    Recurrsion foundation

---
+++ C_programs/Practice/Practice/program504.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void Display()
+{
+    printf("Jay Ganesh..\n");
+    Display();
+}
+int main()
+{
+    Display();
+    return 0;
+}
---
+++ C_programs/Practice/Practice/program505.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void Display()
+{
+   auto int i = 1;
+
+   printf("Jay Ganesh...%d\n",i);
+   i++;
+
+   Display();
+
+}
+int main()
+{
+    Display();
+    return 0;
+}
