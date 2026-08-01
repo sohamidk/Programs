e851552ef978d9d80132a5ba45bb9d3bd5ee8daa
SOham <soham.a.vade@gmail.com>
2026-08-01 09:30:16 +0530

    Creation of shell for CVFS

---
+++ C_programs/CVFS/program631.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[50];
+    sprintf(str,"Jay Ganesh...");
+    
+    return 0;
+}
---
+++ C_programs/CVFS/program633.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[50];
+    int iRet = 0;
+
+    iRet = sprintf(str,"Jay Ganesh..."); 
+
+    printf("Value from iRet is : %d\n",iRet);
+    printf("Data from str is : %s\n",str);
+    
+    return 0;
+}
