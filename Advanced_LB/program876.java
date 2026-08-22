9e6db06c72f2852a9172013cc7453c1aca19a946
SOham <soham.a.vade@gmail.com>
2026-08-22 10:16:00 +0530

    Searching and sorting

---
+++ Advanced_LB/program876.java
@@ -0,0 +1,134 @@
+import java.util.*;
+
+interface GetterSetter
+{
+    void Accept();
+    void Display();
+}
+
+class ArrayX implements GetterSetter
+{
+    protected int Arr[];
+    protected int iSize;
+
+
+    public ArrayX(int iSize)
+    {
+        this.iSize = iSize;
+        Arr = new int[iSize];
+    }
+
+    public void Accept()
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the elements of array");
+        
+        for(int i = 0; i < this.iSize;i++)
+        {
+            Arr[i] = sobj.nextInt();
+        }
+
+        sobj.close();
+    }
+
+    public void Display()
+    {
+        System.out.println("Elements of array are : ");
+        
+        for(int i = 0; i < this.iSize;i++)
+        {
+            System.out.print(Arr[i] + "\t");
+        }
+        System.out.println();
+    }
+}
+
+class Searching extends ArrayX
+{
+    public Searching(int iSize)
+    {
+        super(iSize);
+    }
+
+    public boolean LinearSearch(int iNo)
+    {
+        int i = 0;
+        boolean bFlag = false;
+
+        for(i = 0; i < super.iSize; i++)
+        {
+            if(Arr[i] == iNo)
+            {
+                bFlag = true;
+                break;
+            }
+        }
+
+        return bFlag;
+    }
+
+    public boolean BiDirectionalSearch(int iNo)
+    {
+        int iStart = 0, iEnd = 0;
+        boolean bFlag = false;
+
+        iStart = 0;
+        iEnd = super.iSize - 1;
+
+        while(iStart <= iEnd)
+        {
+            if(Arr[iStart] == iNo || Arr[iEnd] == iNo)
+            {
+                bFlag = true;
+                break;
+            }
+
+            iStart++;
+            iEnd--;
+        }
+
+        return bFlag;
+
+    }
+
+    public boolean CheckSorted()
+    {
+        boolean bFlag = true;
+
+        for(int i = 0 ; i < iSize-1;i++)
+        {
+            if(Arr[i] > Arr[i+1])
+            {
+                bFlag = false;
+                break;
+            }
+        }
+
+        return bFlag;
+    }
+}
+
+
+public class program876
+{
+    public static void main(String[] args) 
+    {
+        Searching sobj = new Searching(5);
+
+        sobj.Accept();
+        sobj.Display();
+
+        if(sobj.CheckSorted())
+        {
+            System.out.println("Array is sorted");
+        }
+        else
+        {
+            System.out.println("Array is not sorted");
+        }
+
+        
+
+    }
+}
---
+++ Advanced_LB/program888.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class program888
+{
+    public static void main(String a[])
+    {
+        Integer Arr[] = {10,13,34,21,15,7,24};
+
+        for(int no : Arr)
+        {
+            System.out.print(no + "\t");
+        }
+
+        System.out.println();
+
+        int index = Arrays.binarySearch(Arr, 27);
+
+
+        if(index >= 0)
+        {
+            System.out.println("Element is present");
+        }
+        else
+        {
+            System.out.println("There is no such element");
+        }
+
+        for(int no : Arr)
+        {
+            System.out.print(no + "\t");
+        }
+
+        System.out.println();
+    }
+}
